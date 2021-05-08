d65b99ecdd182a43d532540cca201c663be598fb
VilhelmKM <vilhelmmoeller@gmail.com>
2021-05-08 13:09:54 +0200

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -144,13 +144,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' # vilhelmmoeller
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'junktestit607@gmail.com'
-EMAIL_HOST_PASSWORD = 'Te123456st'
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
